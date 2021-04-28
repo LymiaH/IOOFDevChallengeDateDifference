110ce70dd441541446198df95acadc767fd17954
LymiaH <LymiaH@users.noreply.github.com>
2021-04-28 16:57:57 +0800

    Make a fat jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,22 @@ application {
     mainClass.set("lymiah.ioof.datediff.MainKt")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+    jar {
+        manifest {
+            attributes(
+                mapOf(
+                    "Main-Class" to application.mainClass
+                )
+            )
+        }
+        from({
+            configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+        }) {
+            exclude("META-INF", "META-INF/**")
+        }
+    }
 }
